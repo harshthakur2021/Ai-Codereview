b845150409f49016edfd48863af13c0d20262710
Harsh Thakur <90609542+harshthakur2021@users.noreply.github.com>
2025-04-24 21:10:50 +0530

    push

--- Backend/server.js
+++ Backend/server.js
@@ -4,10 +4,11 @@ const port = 3000
 
 
 app.get('/', (req, res) => {
-    res.send('Server is Working')
+    res.send('Server is Working fine')
   })
 
   
 app.listen(port, () => {
+  
     console.log(`Example app listening on port ${port}`)
   })
